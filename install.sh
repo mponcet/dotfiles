de4f6201f72cc83666797b77a432861abac6039f
Maxime Poncet <maxime.poncet@gmail.com>
2021-04-21 22:58:10 +0200

    Add alacritty/tmux config files

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 set -o xtrace
 
 # Fedora packages:
-sudo dnf install -y git neovim fzf powerline-go powerline-fonts nodejs
+sudo dnf install -y alacritty tmux git neovim fzf powerline-go powerline-fonts nodejs
 
 # bash:
 cp bashrc ~/.bashrc
@@ -11,3 +11,9 @@ cp bash_profile ~/.bash_profile
 
 # nvim:
 cp -r .config/nvim ~/.config/
+
+# alacritty
+cp -r .config/alacritty ~/.config/
+
+# tmux
+cp .tmux.conf ~/
