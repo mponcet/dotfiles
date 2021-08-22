44fa1e30d420186cefe631e573c4f293bc037334
Maxime Poncet <maxime.poncet@gmail.com>
2021-08-23 00:32:25 +0200

    Test install archlinux

--- install.sh
+++ install.sh
@@ -13,9 +13,32 @@ install_pwndbg() {
 	./setup.sh
 }
 
+install_dnf() {
+	sudo dnf install -y alacritty tmux git neovim fzf powerline-go powerline-fonts nodejs
+}
+
+install_arch() {
+	sudo pacman -S alacritty tmux git neovim fzf nodejs pwndbg
+	sudo yay -S powerline-go
+}
+
 
 # Fedora packages:
-sudo dnf install -y alacritty tmux git neovim fzf powerline-go powerline-fonts nodejs
+distro=$(grep "^ID=" /etc/os-release | cut -d'=' -f2 | sed -e 's/"//g')
+
+case $distro in
+	"fedora")
+		install_dnf
+		install_pwndbg
+		;;
+	"arch")
+		install_arch
+		;;
+	*)
+		echo \"$distro\" is not supported
+		exit 1
+		;;
+esac
 
 # bash:
 cp .bashrc ~/
@@ -30,6 +53,3 @@ cp -r .config/alacritty ~/.config/
 
 # tmux
 cp .tmux.conf ~/
-
-# setup pwndbg
-install_pwndbg
