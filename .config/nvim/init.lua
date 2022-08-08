1d87eee59fa07c6d816b6aeef6f59d323b6ed944
Maxime Poncet <maxime.poncet@gmail.com>
2022-08-08 21:50:02 +0200

    nvim: switch beetween buffers with arrow keys

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -105,6 +105,10 @@ vim.g.maplocalleader = ' '
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
+-- Switch beetween buffers with arrow keys
+vim.keymap.set('n', '<left>', ':bp<CR>')
+vim.keymap.set('n', '<right>', ':bn<CR>')
+
 -- Highlight on yank
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
