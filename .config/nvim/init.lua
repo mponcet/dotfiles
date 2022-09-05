4ecd6adbd432b0dcc8e9f6ba611e2cdc81e19e18
Maxime Poncet <maxime.poncet@gmail.com>
2022-09-05 22:17:21 +0200

    Update rust lsp tools

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -290,42 +290,18 @@ lspconfig.pyright.setup {
 }
 
 -- rust lsp
-local rust_tools_opts = {
-  tools = {
-    autoSetHints = true,
-    hover_with_actions = true,
-    runnables = {
-      use_telescope = true
-    },
-    inlay_hints = {
-      show_parameter_hints = false,
-      parameter_hints_prefix = '',
-      other_hints_prefix = '',
-    },
-  },
+local rt = require("rust-tools")
 
-  -- all the opts to send to nvim-lspconfig
-  -- these override the defaults set by rust-tools.nvim
-  -- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
+rt.setup({
   server = {
-    -- on_attach is a callback called when the language server attachs to the buffer
-    -- on_attach = on_attach,
-    settings = {
-      -- to enable rust-analyzer settings visit:
-      -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
-      ['rust-analyzer'] = {
-        -- enable clippy on save
-        checkOnSave = {
-          command = 'clippy'
-        },
-      }
-    },
-    on_attach = on_attach,
-    capabilities = capabilities,
+    on_attach = function(_, bufnr)
+      -- Hover actions
+      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+      -- Code action groups
+      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+    end,
   },
-}
-
-require('rust-tools').setup(rust_tools_opts)
+})
 
 -- lua lsp
 lspconfig.sumneko_lua.setup {
