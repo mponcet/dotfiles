215e672458e4ac04dfb650e90289489fbce4e637
Maxime Poncet <maxime.poncet@gmail.com>
2022-11-14 22:48:06 +0100

    nvim: update rust-tools

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -305,39 +305,75 @@ lspconfig.pyright.setup {
 }
 
 -- rust lsp
-local opts = {
-    tools = {
-        runnables = {
-            use_telescope = true
-        },
-        inlay_hints = {
-            auto = true,
-            show_parameter_hints = false,
-            parameter_hints_prefix = "",
-            other_hints_prefix = "",
-        },
-    },
+local rt = require("rust-tools")
 
+rt.setup({
+  -- automatically call RustReloadWorkspace when writing to a Cargo.toml file.
+  reload_workspace_from_cargo_toml = true,
+
+  -- These apply to the default RustSetInlayHints command
+  inlay_hints = {
+    -- automatically set inlay hints (type hints)
+    -- default: true
+    auto = true,
+    -- Only show inlay hints for the current line
+    only_current_line = false,
+    -- whether to show parameter hints with the inlay hints or not
+    -- default: true
+    show_parameter_hints = true,
+    -- prefix for parameter hints
+    -- default: "<-"
+    parameter_hints_prefix = "<- ",
+    -- prefix for all the other hints (type, chaining)
+    -- default: "=>"
+    other_hints_prefix = "=> ",
+    -- whether to align to the length of the longest line in the file
+    max_len_align = false,
+    -- padding from the left if max_len_align is true
+    max_len_align_padding = 1,
+    -- whether to align to the extreme right or not
+    right_align = false,
+    -- padding from the right if right_align is true
+    right_align_padding = 7,
+    -- The color of the hints
+    highlight = "Comment",
+  },
+
+  server = {
+    on_attach = function(_, bufnr)
+      on_attach(_, bufnr)
+      -- Hover actions
+      -- vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+      -- Code action groups
+      -- vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+    end,
     -- all the opts to send to nvim-lspconfig
     -- these override the defaults set by rust-tools.nvim
-    -- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
-    server = {
-        -- on_attach is a callback called when the language server attachs to the buffer
-        on_attach = on_attach,
-        settings = {
-            -- to enable rust-analyzer settings visit:
-            -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
-            ["rust-analyzer"] = {
-                -- enable clippy on save
-                checkOnSave = {
-                    command = "clippy"
-                },
-            }
-        }
+    -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+    -- https://rust-analyzer.github.io/manual.html#features
+    settings = {
+      ["rust-analyzer"] = {
+        assist = {
+          importEnforceGranularity = true,
+          importPrefix = "crate"
+        },
+        cargo = {
+          allFeatures = true
+        },
+        checkOnSave = {
+          -- default: `cargo check`
+          command = "clippy"
+        },
+        inlayHints = {
+          lifetimeElisionHints = {
+            enable = true,
+            useParameterNames = true
+          },
+        },
+      },
     },
-}
-
-require('rust-tools').setup(opts)
+  }
+})
 
 -- lua lsp
 lspconfig.sumneko_lua.setup {
