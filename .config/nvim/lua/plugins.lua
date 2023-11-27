2c817b32339b7d67044d31665c40db74b3a2bcfa
Maxime Poncet <maxime.poncet@gmail.com>
2023-11-27 20:53:08 +0100

    leetcode plugin for neovim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -100,4 +100,21 @@ return {
     -- Snippet engine
     'hrsh7th/vim-vsnip'
   },
+  {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    dependencies = {
+        "nvim-telescope/telescope.nvim",
+        "nvim-lua/plenary.nvim", -- required by telescope
+        "MunifTanjim/nui.nvim",
+
+        -- optional
+        "nvim-treesitter/nvim-treesitter",
+        "rcarriga/nvim-notify",
+        "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+        -- configuration goes here
+    },
+  }
 }
