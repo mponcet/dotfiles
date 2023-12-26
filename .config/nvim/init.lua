24981136d2ce21906b939602cff05e63c5c290c1
Maxime Poncet <maxime.poncet@gmail.com>
2023-12-26 20:39:43 +0100

    fix neovim indentation

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -86,6 +86,14 @@ vim.wo.signcolumn = 'yes'
 -- Make line numbers default
 vim.wo.number = true
 
+-- Indentation
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
 -- Set highlight on search
 vim.o.hlsearch = false
 
