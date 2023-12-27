b9b0aa301e81a928a8d78c9e965925620ac6db26
Maxime Poncet <maxime.poncet@gmail.com>
2023-12-27 21:32:21 +0100

    improve install script

--- install.sh
+++ install.sh
@@ -2,8 +2,13 @@
 
 set -o xtrace
 
-FONTS_DIR=~/.local/share/fonts
+if [ -z "${HOME}" ]
+then
+    echo "error: \$HOME undefined"
+    exit 1
+fi
 
+FONTS_DIR=~/.local/share/fonts
 install_fonts() {
 	mkdir -p $FONTS_DIR
 	nerd_fira_code_fonts_url=$(curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest|jq '.assets[] | select(.name == "FiraCode.zip") | .browser_download_url')
@@ -11,19 +16,45 @@ install_fonts() {
 	unzip -o /tmp/FiraCode.zip -d $FONTS_DIR
 }
 
+
+DNF_PACKAGES="curl fzf git jq neovim ripgrep tmux wl-clipboard"
+install_packages() {
+    sudo dnf install -y $DNF_PACKAGES
+}
+
+__copy_dir_clean() {
+    local src=$1
+    local dst=$2
+    rm -rf $dst
+    cp -r $src $dst
+
+}
+
+copy_config_clean() {
+    local src=".config/$1"
+    local dst="$HOME/.config/$1"
+    __copy_dir_clean $src $dst
+}
+
+if ! [[ "$1" == +(personal|bee) ]]
+then
+    echo "$0 personal|bee"
+    exit 0
+else
+    ENV=$1
+fi
+
 install_fonts
+install_packages
 
-cp .bashrc ~/
-cp -r .bashrc.d ~/
-cp .bash_profile ~/
-cp .bash_logout ~/
+cp .bashrc $HOME
+cp -r .bashrc.d $HOME
+cp .bash_profile $HOME
+cp .bash_logout $HOME
+cp .tmux.conf $HOME
+cp $ENV/.gitconfig $HOME
 
-cp .gitconfig ~/
+copy_config_clean nvim
+copy_config_clean alacritty
 
-cp -r .config/nvim ~/.config/
-
-cp -r .config/alacritty ~/.config/
-
-cp .tmux.conf ~/
-
-cp -r .local/bin ~/.local
+cp -r .local/bin/ $HOME/.local
