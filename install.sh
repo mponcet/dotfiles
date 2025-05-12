dad550c6238ad8bbf59edfb4095422860c5a0972
Maxime Poncet <maxime.poncet@gmail.com>
2025-05-12 16:11:22 +0200

    switch to brew for packages install

--- install.sh
+++ install.sh
@@ -8,28 +8,11 @@ then
     exit 1
 fi
 
-FONTS_DIR=~/.local/share/fonts
-install_fonts() {
-	mkdir -p $FONTS_DIR
-	nerd_fira_code_fonts_url=$(curl -L -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest|jq '.assets[] | select(.name == "FiraCode.zip") | .browser_download_url')
-	curl -L -s -o /tmp/FiraCode.zip $nerd_fira_code_fonts_url
-	unzip -o /tmp/FiraCode.zip -d $FONTS_DIR
-}
-
-
-DNF_PACKAGES="curl eza fzf git jq neovim ripgrep wezterm wl-clipboard"
-install_packages() {
-    sudo dnf copr enable wezfurlong/wezterm-nightly -y
-    sudo dnf upgrade -y
-    sudo dnf install -y $DNF_PACKAGES
-}
-
 __copy_dir_clean() {
     local src=$1
     local dst=$2
     rm -rf $dst
     cp -r $src $dst
-
 }
 
 copy_config_clean() {
@@ -46,26 +29,26 @@ else
     ENV=$1
 fi
 
-install_fonts
-install_packages
+# wezterm
+sudo dnf copr enable wezfurlong/wezterm-nightly -y
+sudo dnf install wezterm
 
 # brew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-# rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-# go
-sudo dnf install golang -y
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install eza fzf gcc git neovim ripgrep rustup golang
+brew install --cask font-fira-code-nerd-font
+
+rustup default stable
+rustup-init -y
 go env -w GOPATH=$HOME/.local/share/go
 
 cp .bashrc $HOME
 cp -r .bashrc.d $HOME
 cp .bash_profile $HOME
 cp .bash_logout $HOME
-cp .tmux.conf $HOME
 cp $ENV/.gitconfig $HOME
 
 copy_config_clean nvim
-copy_config_clean alacritty
 copy_config_clean wezterm
 
 cp -r .local/bin/ $HOME/.local
