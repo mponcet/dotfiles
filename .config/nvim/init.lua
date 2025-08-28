7ec4624549e08556332993dc27360f49628359b3
Maxime Poncet <maxime.poncet@gmail.com>
2025-08-28 23:25:37 +0200

    nvim: various updates

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -62,7 +62,7 @@ vim.o.background = 'dark'
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
     callback = function()
-        vim.highlight.on_yank()
+        vim.hl.on_yank()
     end,
     group = highlight_group,
     pattern = '*',
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,10 +2,10 @@ return {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     keys = {
-        { "<leader><space>", ":Telescope buffers<cr>" },
-        { "<leader>ff",      ":Telescope find_files<cr>" },
-        { "<C-p>",           ":Telescope git_files<cr>" },
-        { "<leader>fg",      ":Telescope grep_string<cr>" },
-        { "<leader>km",      ":Telescope keymaps<cr>" }
+        { "<leader><space>", ":Telescope buffers<cr>",    { desc = 'Telescope buffers' } },
+        { "<leader>ff",      ":Telescope find_files<cr>", { desc = 'Telescope find files' } },
+        { "<leader>gf",      ":Telescope git_files<cr>",  { desc = 'Telescop git files' } },
+        { "<leader>fg",      ":Telescope live_grep<cr>",  { desc = 'Telescope live grep' } },
+        { "<leader>km",      ":Telescope keymaps<cr>",    { desc = 'Telescope keymaps' } }
     }
 }
