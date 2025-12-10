ac0fa037d664ddbc7297888224b9634eb412ece8
Maxime Poncet <maxime.poncet@gmail.com>
2025-12-10 22:22:50 +0100

    nvim: typescript lsp

--- .config/nvim/lsp/typescript.lua
+++ .config/nvim/lsp/typescript.lua
@@ -1,6 +1,13 @@
 return {
-    cmd = { vim.fn.stdpath('data') .. '/mason/bin/typescript-language-server' },
-    root_markers = { 'tsconfig.json' },
-    filetypes = { 'typescript' },
+    cmd = { vim.fn.stdpath('data') .. '/mason/bin/vtsls', '--stdio' },
+    root_markers = { 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 'bun.lockb', 'bun.lock' },
+    filetypes = {
+        'javascript',
+        'javascriptreact',
+        'javascript.jsx',
+        'typescript',
+        'typescriptreact',
+        'typescript.tsx',
+    },
     single_file_support = true,
 }
